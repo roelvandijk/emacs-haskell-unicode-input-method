ed8f96a577ae0aa32972feaae189bfab440ad0b4
Roel van Dijk <vandijk.roel@gmail.com>
2011-02-13 20:35:28 +0100

    (provide 'haskell-unicode-input-method) so that you can require it
    Ignore-this: a83b7654975a704e99e8019552c56e6e
    
    darcs-hash:20110213193528-cab80-aa42671ab62552f98711ab3d3084bc40cc90219e.gz

--- haskell-unicode-input-method.el
+++ haskell-unicode-input-method.el
@@ -34,6 +34,7 @@
 ;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 ;; OF THE POSSIBILITY OF SUCH DAMAGE.
 
+(provide 'haskell-unicode-input-method)
 (require 'quail)
 
 (quail-define-package
